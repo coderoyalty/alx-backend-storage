12bd053c4fb9bec88d9de073505408006a9d3ae6
Akanni Emmanuel <akannie33@gmail.com>
2023-08-16 04:46:40 -0700

    ✅ 0x02: task 1 done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,12 +3,13 @@
 exercise file
 """
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 import uuid
 
 
 class Cache:
     """cache class"""
+
     def __init__(self):
         """ initialize cache"""
         self._redis = redis.Redis()
@@ -21,3 +22,25 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """get data from cache"""
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_int(self, key: str) -> str:
+        """automatically parametrize Cache@get_int"""
+        value = self.get(key)
+        try:
+            value = int(value.decode())
+        except Exception:
+            value = 0
+        return value
+
+    def get_str(self, key: str) -> str:
+        """automatically parametrize Cache@get_str"""
+        value = self.get(key)
+        return value.decode()
